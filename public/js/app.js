adce9be5b17f826a72af2ef07b2003062cd185d9
Harshil Kotadiya <harshilkotadiya002@gmail.com>
2023-09-01 09:46:34 +0530

    changed app.js

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit',(e)=>{
     messageTwo.textContent = ''
     messageThree.textContent = ''
 
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
     .then((response)=>{
         response.json().then((data)=>{
             if(data.error){
